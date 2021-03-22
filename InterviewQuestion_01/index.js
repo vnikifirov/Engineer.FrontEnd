e72a60101826ca24fa339c77ea392d473d10c5e6
Vadim <39588115+vnikifirov@users.noreply.github.com>
2021-03-22 14:56:26 +0300

    Update comment

--- InterviewQuestion_01/index.js
+++ InterviewQuestion_01/index.js
@@ -1,4 +1,4 @@
-// Error: JavaScript closure inside loops – simple practical example
+// Error: i am getting five times five instead of a series of numbers from zero to four?
 // Solution: https://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
 for (var i = 0; i < 5; i++) {
   setTimeout(function () {
